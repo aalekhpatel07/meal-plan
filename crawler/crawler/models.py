08d4f97ae7da91aa3a408394d18f3acd86731d63
Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
2023-07-30 02:54:16 -0500

    Sprinkle statsd.
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>

--- crawler/crawler/models.py
+++ crawler/crawler/models.py
@@ -1,3 +1,5 @@
+import urllib.parse
+
 import msgpack
 import pydantic
 import typing
@@ -7,6 +9,14 @@ class Link(pydantic.BaseModel):
     url: str
     metadata: typing.Optional[typing.Dict[str, typing.Any]] = None
 
+    _hostname: typing.Optional[str] = None
+
+    @property
+    def hostname(self) -> str:
+        if self._hostname is None:
+            self._hostname = urllib.parse.urlparse(self.url).hostname or 'unknown'
+        return self._hostname
+
 
 class CrawlResult(pydantic.BaseModel):
     url: str
@@ -14,6 +24,13 @@ class CrawlResult(pydantic.BaseModel):
     contents: typing.Optional[bytes] = None
     status_code: typing.Optional[int] = None
     timestamp: typing.Optional[str] = None
+    _hostname: typing.Optional[str] = None
+
+    @property
+    def hostname(self) -> str:
+        if self._hostname is None:
+            self._hostname = urllib.parse.urlparse(self.url).hostname or 'unknown'
+        return self._hostname
 
     @classmethod
     def from_msgpack(cls, value):
